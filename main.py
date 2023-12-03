ea45b0f020a171828093b67c9e09749b7d9678ac
MartinsGuilherme <guilherme.martins@nappsolutions.com>
2023-12-03 16:12:13 -0300

    feat: normalization before detect

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import time
 from random import randrange as rand
 from pathlib import Path
 import service
+from torchvision.models.detection.ssd import SSDClassificationHead
+from torchvision.models.detection import _utils
+
 
 if __name__ == '__main__':
 
@@ -20,14 +23,28 @@ if __name__ == '__main__':
     if Path(__file__).parent.joinpath('model_local.pt').exists():
         model_local = torch.load('model_local.pt')
     else: 
+        size = 600
         model_local = torchvision.models.detection.ssd300_vgg16(weights=None,num_classes=2)
+        # Retrieve the list of input channels. 
+        in_channels = _utils.retrieve_out_channels(model_local.backbone, (size, size))
+        # List containing number of anchors based on aspect ratios.
+        num_anchors = model_local.anchor_generator.num_anchors_per_location()
+        # The classification head.
+        model_local.head.classification_head = SSDClassificationHead(
+            in_channels=in_channels,
+            num_anchors=num_anchors,
+            num_classes=2,
+        )
+        # Image size for transforms.
+        model_local.transform.min_size = (size,)
+        model_local.transform.max_size = size
 
     # Treinar o model COCO para imagens locais
     path = Path(__file__).parent.joinpath('data')
-    #model = service.train(model, str(path.joinpath('person').absolute()))
+    model = service.train(model, str(path.joinpath('person').absolute()), 0)
 
     # Treinar o model local para imagens heads
-    model_local = service.train(model_local, str(path.joinpath('head').absolute()), 100)
+    model_local = service.train(model_local, str(path.joinpath('head').absolute()), 25)
     
     # Capturar um print do monitor
     printScreen = ImageGrab.grab()
@@ -41,6 +58,8 @@ if __name__ == '__main__':
     with torch.no_grad():
         model.eval()
         output = model(image_tensor)
+        model_local.eval()
+        output_local = model_local(image_tensor)
 
     # Extrair as caixas delimitadoras, os rótulos e os escores da saída do modelo
     boxes = output[0]['boxes'].int().numpy()
@@ -52,24 +71,21 @@ if __name__ == '__main__':
     _, x = service.untransform_and_draw_boxes(image_tensor[0], boxes, labels, scores, threshold=0.5, save_as='person')
     print(f"Persons: {len(x)}")
 
-    with torch.no_grad():
-        model_local.eval()
-        output = model_local(image_tensor)
-    
-    boxes = output[0]['boxes'].int().numpy()
-    labels = output[0]['labels'].numpy()
-    scores = output[0]['scores'].numpy()
+        
+    boxes_local = output_local[0]['boxes'].int().numpy()
+    labels_local = output_local[0]['labels'].numpy()
+    scores_local = output_local[0]['scores'].numpy()
     
     print(f'Detections: {len(labels)}')
 
-    image_with_boxes, x = service.untransform_and_draw_boxes(image_tensor[0], boxes, labels, scores, threshold=0.1, save_as='head')
+    image_with_boxes, x = service.untransform_and_draw_boxes(image_tensor[0], boxes_local, labels_local, scores_local, threshold=0.25, save_as='head')
     print(f"Heads: {len(x)}")
 
     # salvar a imagem com as caixas delimitadoras
     # cv2.imshow('Image with boxes',  image_with_boxes)
     cv2.imwrite('Image_with_boxes.jpeg', image_with_boxes, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
     #save the models
     torch.save(model, 'model.pt')
--- service.py
+++ service.py
@@ -8,6 +8,11 @@ import time
 from random import randrange as rand
 from pathlib import Path
 import os
+import albumentations as A
+import random
+
+LR = 0.00035
+SAMPLES_QUANTITY = 500
 
 # Definir as classes do modelo "COCO"
 classes = ['background', 'person', 'bicycle', 'car', 'motorcycle',
@@ -32,18 +37,25 @@ classes = ['background', 'person', 'bicycle', 'car', 'motorcycle',
 
 # Definir uma função para transformar a imagem em um tensor
 def transform(image : cv2.typing.MatLike):
-    # Redimensionar a imagem para 300x300 pixels
-    #image = cv2.resize(image, (600, 600))
+    # Redimensionar a imagem para maximo 600x600 pixels
+    image = cv2.resize(image, (600, 600))
     # Converter a imagem em um tensor
     image = torch.from_numpy(image).float()
-    # Normalizar a imagem
-    image = image / 255.0
     # Transpor a imagem para o formato CxHxW
     image = image.permute(2, 0, 1)
+    # Normalizar a imagem
+    image = image / 255.0
+    normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                                 std=[0.229, 0.224, 0.225])
+    transform = torchvision.transforms.Compose([normalize])
+
+    image = transform(image)
+    
     # Adicionar uma dimensão de lote
     #transformation = torchvision.transforms.ToTensor()
     #image = transformation(image)
     image = image.unsqueeze(0)
+    
     return image
 
 # Definir uma função para desenhar as caixas delimitadoras na imagem
@@ -68,8 +80,11 @@ def untransform_and_draw_boxes(tensor_image: torch.Tensor, boxes : tuple, labels
             x1, y1, x2, y2 = box
             # Se for person separar a miniimagem
             if label == 1:
-                Path(__file__).parent.joinpath("data").joinpath(save_as).mkdir(parents=True, exist_ok=True)
-                cv2.imwrite(f'{Path(__file__).parent.joinpath("data").joinpath(save_as).joinpath(f"{save_as}_{int(time.time())+rand(1,100,1)}.jpeg")}', original_image[y1:y2, x1:x2])
+                Path(__file__).parent.joinpath("data").joinpath("detected").joinpath(save_as).mkdir(parents=True, exist_ok=True)
+                if x2>x1 and y2>y1:
+                    cv2.imwrite(f'{Path(__file__).parent.joinpath("data").joinpath("detected").joinpath(save_as).joinpath(f"{save_as}_{int(10000*score)}_{int(time.time()+random.randint(1000,9999))}.jpeg")}', original_image[y1:y2, x1:x2])
+                else:
+                    print(f"Invalid box: {box}")
                 are_there_persons.append(box)
             # Desenhar a caixa na imagem
             image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
@@ -84,46 +99,106 @@ def train(model : torchvision.models.detection.ssd.SSD, file_dir ='C://Users//gm
         model.to(device)
         print(f"Running on {device}")
         print(f"Allocated memory: {torch.cuda.memory_allocated(device)}")
-        
+    augmentator = A.Compose([
+            A.Blur(blur_limit=3, p=0.1),
+            A.MotionBlur(blur_limit=3, p=0.1),
+            A.MedianBlur(blur_limit=3, p=0.1),
+            A.ToGray(p=0.3),
+            A.RandomBrightnessContrast(p=0.3),
+            A.ColorJitter(p=0.3),
+            A.RandomGamma(p=0.3),
+            A.HorizontalFlip(p=0.1)],
+              bbox_params= A.BboxParams(format='pascal_voc', 
+                                        label_fields=['labels'])
+        )
+
     # Transforms
-    resize = torchvision.transforms.Resize((300, 300))
+    resize = torchvision.transforms.Resize((600, 600), antialias=False)
     to_tensor = torchvision.transforms.ToTensor()
     normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     trans = torchvision.transforms.Compose([to_tensor, resize, normalize])
-    files = os.listdir(file_dir)
     
     model.train()
-
+    
     # Define the optimizer
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.95, weight_decay=0.0005)
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=LR, momentum=0.9, nesterov=True)
     
     for j in range(epochs):
         print(f"Epoch: {j+1} / {epochs}")
         ima = []
         target = []
-        for file in files:
+        bbox = []
+        with open(Path(__file__).parent.joinpath('data').joinpath("annotations").joinpath("list_bbox_celeba.txt"), "r" ) as txt_file:
+            bbox = txt_file.readlines()
+        txt_file.close()
+        bbox = {x.split()[0]: x.split()[1:]  for x in bbox}
+        bbox = {k: [int(v[0]), int(v[1]), int(v[0])+int(v[2]), int(v[1])+int(v[3])] for k, v in bbox.items() if k != "image_id" and k != "202599"} 
+        samples = os.listdir(file_dir)
+        random.shuffle(samples)
+        samples = samples[0:SAMPLES_QUANTITY]
+        for j, file in enumerate(samples):
+            if j%100 == 0:
+                print(f"Progress: {j} / {len(samples)} Next sample: {file} Bbox: {bbox.get(file, [0,0,0,0])}")
             ima_= cv2.imread(file_dir + '//' + file)
             original_dimension = ima_.shape 
+            
+            x1, y1, x2, y2 = int(original_dimension[0]*0.05), int(original_dimension[1]*0.05), int(original_dimension[0]*0.95), int(original_dimension[1]*0.95)
+            
+            if file[0:8] == "anotated":
+                x1=int(file.split("-")[1])
+                y1=int(file.split("-")[2])
+                x2=int(file.split("-")[3])
+                y2=int(file.split("-")[4])
+
+            x1, y1, x2, y2 = bbox.get(file, [x1, y1, x2, y2])
+
+            """ try:
+                augmented_image = augmentator(image=ima_, bboxes=[[x1,y1,x2,y2]], labels=[1])
+                x1, y1, x2, y2 = augmented_image["bboxes"][0]
+            except: """
+            augmented_image = {"image":ima_}
+
+            new_image = trans(augmented_image["image"])
+            new_dimension = new_image.shape
+
+            # bbox validation
+            x1, y1, x2, y2 = int(new_dimension[1]*x1/original_dimension[0]), int(new_dimension[2]*y1/original_dimension[1]), int(new_dimension[1]*x2/original_dimension[0]), int(new_dimension[2]*y2/original_dimension[1])
+            if x2<=x1 or y2<=y1:
+                print(f"Invalid box: {file}")
+                continue
+
             # add to batch and send to gpu
-            ima.append(trans(ima_).to(device))
-            target.append({"boxes":torch.tensor(data=[[0,0,300,300]]).to(device), "labels":torch.tensor(data=[1]).to(device)})
+            ima.append(new_image.to(device))
+            
+            target.append({"boxes":torch.tensor(data=[[x1,y1,x2,y2]]).to(device), 
+                            "labels":torch.tensor(data=[1]).to(device)})
             # max_batch_size = 8
             if len(ima) == 8:
                 optimizer.zero_grad()
-                output = model.forward(images = ima, targets = target) 
-                loss = output.get('bbox_regression', None)
-                if loss is not None:
-                    loss.backward()
-                    optimizer.step()
+                output = model(images = ima, targets = target) 
+                losses = output.get('bbox_regression', None)
+                
+                if losses is not None:
+                    loss = sum(loss for loss in output.values())
+                    if not torch.isnan(loss).any():
+                        loss.backward()
+                        optimizer.step()
                 ima = []
                 target = []
         # residual
-        optimizer.zero_grad()
-        output = model.forward(images = ima, targets = target) 
-        loss = output.get('bbox_regression', None)
-        if loss is not None:
-            loss.backward()
-            optimizer.step()
+        if len(ima) > 0:
+            optimizer.zero_grad()
+            output = model(images = ima, targets = target) 
+            losses = output.get('bbox_regression', None)
+            if losses is not None:
+                loss = sum(loss for loss in output.values())
+                if not torch.isnan(loss).any():
+                    loss.backward()
+                    optimizer.step()
+            ima = []
+            target = []
+        print(f"Loss: {loss}")
     model.to(old_device)
     model.eval()
     return model
